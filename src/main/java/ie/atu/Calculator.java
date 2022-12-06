e1930b5730f575da4579337e97ae8c0d994ee10d
g00387170 <g00387170@atu.ie>
2022-12-06 19:48:31 +0000

    Adding files including yaml

---
+++ src/main/java/ie/atu/Calculator.java
@@ -0,0 +1,21 @@
+package ie.atu;
+
+public class Calculator {
+    private int t;
+
+    public Calculator(){
+        t = 0;
+    }
+
+    public int add(int one, int two){
+        t = one + two;
+        return t;
+    }
+
+    public int subtract(int one, int two)
+    {
+        t = one - two;
+        return t;
+    }
+
+}
---
+++ src/test/java/ie/atu/CalculatorTest.java
@@ -0,0 +1,30 @@
+package ie.atu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    Calculator myCalc;
+
+    @BeforeEach
+    void setUp() {
+        myCalc = new Calculator();
+    }
+
+    @Test
+    void testAdd(){
+        assertEquals(9, myCalc.add(4,5));
+    }
+
+    @Test
+    void testSubtract(){
+        assertEquals(5, myCalc.subtract(10,5));
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
